c60a2345a5fe520379c15a0d5bc393df45040dfa
Tim Howard <tghoward@gmail.com>
2016-10-24 13:51:59 -0400

    add comments

--- initialTests.py
+++ initialTests.py
@@ -2,44 +2,44 @@
 """
 Created on Fri Oct 21 14:42:24 2016
 
-@author: NYNHP_admin
+@author: Tim Howard
 """
 
 #%%
 # setup
 import arcpy
-from arcpy import env as E
+from arcpy import env as ENV
 import arcpy.sa as SA
 
-E.workspace = "D:/EPA_AdjArea/CalcAdjArea"
-E.overwriteOutput=True
+ENV.workspace = "D:/EPA_AdjArea/CalcAdjArea"
+ENV.overwriteOutput=True
 
 arcpy.CheckOutExtension("Spatial")
 
-#%%
+arcpy.ImportToolbox("C:\\Program Files\\TauDEM\\TauDEM5Arc\\TauDEM Tools.tbx", "TauDEM")
 
-# start with the sample points
+#%%
+# start with the sample points, buffer them
 pointLoc = "D:/EPA_AdjArea/CalcAdjArea/NYW14_testPts.gdb"
 pointLayer = "SitePoints"
 inPoints = pointLoc + "/" + pointLayer
 
-outBuff = pointLoc + "/" + "SitePtsBuff1km"
+buffedPts = pointLoc + "/" + "SitePtsBuff1km"
 buffDist = "1000"
 
-arcpy.Buffer_analysis(inPoints, outBuff, buffDist, "FULL","ROUND","NONE")
+arcpy.Buffer_analysis(inPoints, buffedPts, buffDist, "FULL","ROUND","NONE")
 
 #%%
 # get a list of ObjectIDs for all records, just to be sure
-cursor = arcpy.SearchCursor(outBuff)
+cursor = arcpy.SearchCursor(buffedPts)
 idList = []
 for row in cursor:
     idval = row.getValue("OBJECTID")
     idList.append(idval)
     
 #%%
-
-
-arcpy.MakeFeatureLayer_management(outBuff, "lyr")
+# extract a separate raster for each buffered point
+arcpy.MakeFeatureLayer_management(buffedPts, "lyr")
 
 inRas = "D:/EPA_AdjArea/CalcAdjArea/tauDEMtests/flowdir.tif"
 outPath = "D:\\EPA_AdjArea\\CalcAdjArea\\output"
@@ -55,26 +55,18 @@ for i in idList:
     outExtractByMask.save(outname)
 
 #%%
-need to use MakeFeatureLayer_management
-and then Select layer by attribute or something 
 
-inRas = "D:/EPA_AdjArea/CalcAdjArea/tauDEMtests/flowdir.tif"
-outPath = "D:\\EPA_AdjArea\\CalcAdjArea\\output"
+flowDirGrid = "D:\\EPA_AdjArea\\CalcAdjArea\\output\\nyw14-007"
+OutContribAreaGrid = "D:\\EPA_AdjArea\\CalcAdjArea\\output\\nyw14-007-contrib"
+inPointFC = "D:\\EPA_AdjArea\\CalcAdjArea\\NYW14_testPts.gdb\\demToPoint_clip_ID"
 
-cursor = arcpy.SearchCursor(outBuff)
-for row in cursor:
-    polName = row.getValue("site_ID")
-    print(polName)
-    outExtractByMask = SA.ExtractByMask(inRas, row)
-    outname = outPath + "\\" + polName
-    outExtractByMask.save(outname)
-    
+
+
+arcpy.AreaDinf_TauDEM(flowDirGrid, inPointFC, "", "false", 8, OutContribAreaGrid)
     
     
     
 #%%
     
     
-#row = cursor.next()
-#while row:
     
